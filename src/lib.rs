ad57b85382ad654c070b42ee37337a95fef30e02
Kun Qin <kuqin@microsoft.com>
2026-02-13 11:49:42 -0800

    Define MEMORY_SP bit in paging crate
    This change adds the special-purpose memory region bit in paging crate.
    
    This bit may be used for specific device drivers or applications.
    
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

--- src/lib.rs
+++ src/lib.rs
@@ -151,7 +151,7 @@ bitflags! {
         const ReadProtect       = 0x00000000_00002000u64;   // Maps to Present bit on X64
         const ExecuteProtect    = 0x00000000_00004000u64;   // Maps to NX bit on X64
         const ReadOnly          = 0x00000000_00020000u64;   // Maps to Read/Write bit on X64
-
+        const SpecialPurpose    = 0x00000000_00040000u64;   // Special-purpose memory, maps to supervisor bit on X64
 
         const CacheAttributesMask = Self::Uncached.bits() |
                                    Self::WriteCombining.bits() |
